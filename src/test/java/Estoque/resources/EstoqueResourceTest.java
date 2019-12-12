c8df1623a8efcd03a32672299a8c424936388726
lazaro marinho <lazaro.marinho@radixeng.com.br>
2019-12-12 12:58:28 -0300

    adicionando dependecia

--- src/test/java/Estoque/resources/EstoqueResourceTest.java
+++ src/test/java/Estoque/resources/EstoqueResourceTest.java
@@ -1,31 +1,31 @@
-//package Estoque.resources;
-//
-//import static org.junit.Assert.*;
-//
-//import org.dbunit.DBTestCase;
-//import org.dbunit.dataset.IDataSet;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import Estoque.db.EstoqueDao;
-//
-//public class EstoqueResourceTest extends DBTestCase {
-//
-//	private EstoqueDao estoqueDao;
-//
-//	@Before
-//	public void setUp() throws Exception {
-//	}
-//
-//	@Test
-//	public void testGetProduto() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Override
-//	protected IDataSet getDataSet() throws Exception {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//}
+package Estoque.resources;
+
+import static org.junit.Assert.*;
+
+import org.dbunit.DBTestCase;
+import org.dbunit.dataset.IDataSet;
+import org.junit.Before;
+import org.junit.Test;
+
+import Estoque.db.EstoqueDao;
+
+public class EstoqueResourceTest extends DBTestCase {
+
+	private EstoqueDao estoqueDao;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetProduto() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
